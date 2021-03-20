e81b37b046b47dad86a91d0bbcc546969b9c5684
ramadan82 <ramadan.isa@gmail.com>
2021-03-20 18:22:40 +0000

    added bg image and logo

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Typed from "react-typed"
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>End to End Software and Web Development Solutions</h1>
+                <Typed 
+                className="typed-text"
+                strings={["web design", "web development", "mobile applications development", "Business software solutions"]}
+                typedSpeed={4}
+                backSpeedd={60}
+                loop
+                />
+                <a href="#" className="btn-main-offer" >Contact Me</a>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
