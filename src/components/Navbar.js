1a0e2b6bf14701f29d750485178a4f4b065793c2
ramadan82 <ramadan.isa@gmail.com>
2021-03-20 19:07:14 +0000

    added contact us

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
         <a className="nav-link" href="#">Projects</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Contact Us</a>
+        <a className="nav-link" href="#">Contact</a>
       </li>
     </ul>
     </div>
